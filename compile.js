2f00df2ada73548be24d587d1713a6dbac28d849
jelybeansenjoyer <therealraghavkashyap26@gmail.com>
2022-12-03 12:25:53 +0530

    changes lottery deploy.js

--- compile.js
+++ compile.js
@@ -2,6 +2,6 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname,'contracts','Inbox.sol');
-const source = fs.readFileSync(inboxPath,'utf8');
-module.exports = solc.compile(source,1).contracts[':Inbox'];
+const lotteryPath = path.resolve(__dirname,'contracts','Lottery.sol');
+const source = fs.readFileSync(lotteryPath,'utf8');
+module.exports = solc.compile(source,1).contracts[':Lottery'];
--- deploy.js
+++ deploy.js
@@ -9,7 +9,7 @@ const web3 = new Web3(provider);
 const deploy = async ()=>{
     const account = await web3.eth.getAccounts();
     console.log('Attempting to deploy an account ', account[0] );
-    const result = await new web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode , arguments:['Hi There!']}).send({gas:'1000000' , from:account[0]});
+    const result = await new web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode}).send({gas:'1000000' , from:account[0]});
     console.log('Contract deployed to ',result.options.address);
 };
 deploy();
